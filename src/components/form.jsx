ff19c17fa1dcf31295c900deb85a4b3e7b466137
Adalyne Cummins <adalynecummins@gmail.com>
2022-10-26 09:20:30 -0400

    form now function

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,35 +1,20 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-class Form extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      firstName: '',
-      lastName: ''
-    };
-  }
+function Form() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
 
-  handleSubmit = (event) => {
-    alert(`Hello ${this.state.firstName} ${this.state.lastName}!`)
-  }
-
-  handleChange = (event) => {
-    this.setState({[event.target.name]: event.target.value});
-  }
-
-  render() {
-    return (
-      <form className="form" onSubmit={this.handleSubmit}>
-        <label>First Name
-          <input name="firstName" type="text" value={this.state.firstName} onChange={this.handleChange} />
-        </label>
-        <label>Last Name
-          <input name="lastName" type="text" value={this.state.lastName} onChange={this.handleChange} />
-        </label>
-        <input className="button" type="submit" value="Greet Me" />
-      </form>
-    );
-  }
+  return (
+    <form className="form" onSubmit={() => alert(`Hello ${this.state.firstName} ${this.state.lastName}!`)}>
+      <label>First Name
+        <input name="firstName" type="text" value={firstName} onChange={() => setFirstName} />
+      </label>
+      <label>Last Name
+        <input name="lastName" type="text" value={lastName} onChange={() => setLastName} />
+      </label>
+      <input className="button" type="submit" value="Greet Me" />
+    </form>
+  );
 }
 
 export default Form;
